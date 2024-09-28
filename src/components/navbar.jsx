54954c1573aff8023240dbb47a2d8aa4f5889238
Anirudh Dattu <anirudh.dattu.22cse@bmu.edu.in>
2024-09-28 14:46:14 +0530

    update landing page

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,22 +6,29 @@ const Header = () => {
   return (
     <header>
       <div className="flex justify-center items-center p-5 bg-black text-white">
-        <div className="relative w-full max-w-7xl flex justify-between items-center">
+        <div className="relative w-full max-w-7xl flex justify-between items-center pb-5">
           <div className="flex-1 flex justify-center">
             <Link to="/">
-              <img src={logo} alt="Logo" className="h-auto w-auto max-h-20" />
+              <img
+                src={logo}
+                alt="Logo"
+                className="h-auto w-auto max-h-[70px]"
+              />
             </Link>
           </div>
-          <div className="absolute right-0 flex space-x-6">
-            <a href="#footer" className="text-lg hover:underline">
-              Contact
-            </a>
+          <div className="absolute right-0 gap-7 flex space-x-6">
             <a
               href="https://drive.google.com/file/d/1Xd_E5VAKKU2SH8KdCyczU7mTj50Nyfej/view?usp=sharing"
-              className="text-lg hover:underline"
+              className="text-xl hover:text-[#BF81F0] font-play tracking-widest"
               target="_blank"
             >
               Resume
+            </a>
+            <a
+              href="#footer"
+              className="text-xl hover:text-[#BF81F0] font-play tracking-widest"
+            >
+              Contact
             </a>
           </div>
         </div>
